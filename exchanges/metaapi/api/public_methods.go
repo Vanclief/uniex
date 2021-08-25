10f9ff4795205bc3ae3e52a9bf57760c9fd64e4b
Franco Valencia <franco.avalencia@gmail.com>
2021-08-25 16:40:56 -0500

    Added missing Ticker method to MetaAPI

--- exchanges/metaapi/api/public_methods.go
+++ exchanges/metaapi/api/public_methods.go
@@ -8,6 +8,11 @@ import (
 	"github.com/vanclief/finmod/market"
 )
 
+func (api *API) GetTicker(pair *market.Pair) (*market.Ticker, error) {
+	const op = "MetaAPI.GetHistoricalData"
+	return nil, ez.New(op, ez.ENOTIMPLEMENTED, "Not implemented", nil)
+}
+
 func (api *API) GetHistoricalData(pair *market.Pair, start, end time.Time) ([]market.Candle, error) {
 	const op = "MetaAPI.GetHistoricalData"
 	pairString := strings.ToUpper(pair.Base.Symbol + pair.Quote.Symbol)
