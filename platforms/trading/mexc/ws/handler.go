f285f332c8ef8fe7e554e68432ac943396000c07
Franco Valencia <franco.avalencia@gmail.com>
2022-03-13 09:09:51 -0600

    Fixed MEXC wrong filtering problem

--- platforms/trading/mexc/ws/handler.go
+++ platforms/trading/mexc/ws/handler.go
@@ -13,8 +13,8 @@ import (
 
 type MEXCHandler struct {
 	opts genericws.HandlerOptions
-	Ask  map[string]market.OrderBookRow
-	Bid  map[string]market.OrderBookRow
+	Asks map[string]market.OrderBookRow
+	Bids map[string]market.OrderBookRow
 }
 
 func NewHandler() *MEXCHandler {
@@ -23,117 +23,9 @@ func NewHandler() *MEXCHandler {
 
 func (h *MEXCHandler) Init(opts genericws.HandlerOptions) error {
 	h.opts = opts
+	h.Asks = make(map[string]market.OrderBookRow)
+	h.Bids = make(map[string]market.OrderBookRow)
 	return nil
-}
-
-func (h *MEXCHandler) UpdateOrderBook(data MEXCOrderBookData) {
-	for _, v := range data.Asks {
-		if v[1] > 0 {
-			h.Ask = market.OrderBookRow{
-				Price:       v[0],
-				Volume:      v[1],
-				AccumVolume: v[1],
-			}
-		}
-	}
-
-	for _, v := range data.Bids {
-		if v[1] > 0 {
-			h.Bid = market.OrderBookRow{
-				Price:       v[0],
-				Volume:      v[1],
-				AccumVolume: v[1],
-			}
-		}
-	}
-}
-
-func mexcPairToMarketPair(in string) (market.Pair, error) {
-	const op = "mexcPairToMarketPair"
-	pairArray := strings.Split(in, "_")
-	if len(pairArray) != 2 {
-		return market.Pair{}, ez.New(op, ez.EINVALID, "invalid pair", nil)
-	}
-	return market.Pair{
-		Base:  market.Asset{Symbol: pairArray[0]},
-		Quote: market.Asset{Symbol: pairArray[1]},
-	}, nil
-}
-
-func (h *MEXCHandler) Parse(in []byte) (*ws.ListenChan, error) {
-
-	t := MEXCMsg{}
-	err := json.Unmarshal(in, &t)
-	if err != nil {
-		return nil, err
-	}
-
-	switch t.Channel {
-	case "push.ticker":
-		return h.ToTickers(in)
-	case "push.depth":
-		return h.ToOrderBook(in)
-	}
-
-	return nil, nil
-}
-
-func (h *MEXCHandler) ToTickers(in []byte) (*ws.ListenChan, error) {
-	const op = "MEXCHandler.ToTickers"
-	payload := MEXCTickerPayload{}
-
-	err := json.Unmarshal(in, &payload)
-	if err != nil {
-		return nil, ez.New(op, ez.EINVALID, "Failed to unmarshal payload", err)
-	}
-
-	pair, err := mexcPairToMarketPair(payload.Data.Symbol)
-	if err != nil {
-		return nil, ez.Wrap(op, err)
-	}
-
-	marketTicker := market.Ticker{
-		Time:   payload.Timestamp,
-		Ask:    payload.Data.Ask1,
-		Bid:    payload.Data.Bid1,
-		Last:   payload.Data.LastPrice,
-		Volume: 0,
-		VWAP:   0,
-	}
-	return &ws.ListenChan{
-		Pair:    pair,
-		Tickers: []market.Ticker{marketTicker},
-	}, nil
-}
-
-func (h *MEXCHandler) ToOrderBook(in []byte) (*ws.ListenChan, error) {
-	const op = "MEXCHandler.ToOrderBook"
-	payload := MEXCOrderBookPayload{}
-
-	err := json.Unmarshal(in, &payload)
-	if err != nil {
-		return nil, ez.New(op, ez.EINVALID, "Failed to unmarshal payload", err)
-	}
-
-	pair, err := mexcPairToMarketPair(payload.Symbol)
-	if err != nil {
-		return nil, ez.Wrap(op, err)
-	}
-
-	h.UpdateOrderBook(payload.Data)
-
-	if h.Ask.Price == 0 || h.Bid.Price == 0 {
-		return nil, nil
-	}
-
-	return &ws.ListenChan{
-		Pair: pair,
-		OrderBook: market.OrderBook{
-			Time: time.Now().Unix(),
-			Asks: []market.OrderBookRow{h.Ask},
-			Bids: []market.OrderBookRow{h.Bid},
-		},
-	}, nil
 }
 
 func (h *MEXCHandler) GetSettings() (genericws.Settings, error) {
@@ -180,6 +72,129 @@ func (h *MEXCHandler) VerifySubscriptionResponse(in []byte) error {
 		return nil
 	}
 	return ez.New(op, ez.EINVALID, "invalid subscription response", nil)
+}
+
+func (h *MEXCHandler) Parse(in []byte) (*ws.ListenChan, error) {
+
+	t := MEXCMsg{}
+	err := json.Unmarshal(in, &t)
+	if err != nil {
+		return nil, err
+	}
+
+	switch t.Channel {
+	case "push.ticker":
+		return h.toTickers(in)
+	case "push.depth":
+		return h.toOrderBook(in)
+	}
+
+	return nil, nil
+}
+
+func (h *MEXCHandler) toTickers(in []byte) (*ws.ListenChan, error) {
+	const op = "MEXCHandler.toTickers"
+	payload := MEXCTickerPayload{}
+
+	err := json.Unmarshal(in, &payload)
+	if err != nil {
+		return nil, ez.New(op, ez.EINVALID, "Failed to unmarshal payload", err)
+	}
+
+	pair, err := mexcPairToMarketPair(payload.Data.Symbol)
+	if err != nil {
+		return nil, ez.Wrap(op, err)
+	}
+
+	marketTicker := market.Ticker{
+		Time:   payload.Timestamp,
+		Ask:    payload.Data.Ask1,
+		Bid:    payload.Data.Bid1,
+		Last:   payload.Data.LastPrice,
+		Volume: 0,
+		VWAP:   0,
+	}
+	return &ws.ListenChan{
+		Pair:    pair,
+		Tickers: []market.Ticker{marketTicker},
+	}, nil
+}
+
+func (h *MEXCHandler) toOrderBook(in []byte) (*ws.ListenChan, error) {
+	const op = "MEXCHandler.toOrderBook"
+	payload := MEXCOrderBookPayload{}
+
+	err := json.Unmarshal(in, &payload)
+	if err != nil {
+		return nil, ez.New(op, ez.EINVALID, "Failed to unmarshal payload", err)
+	}
+
+	pair, err := mexcPairToMarketPair(payload.Symbol)
+	if err != nil {
+		return nil, ez.Wrap(op, err)
+	}
+
+	h.updateOrderBook(payload)
+
+	ask, ok := h.Asks[payload.Symbol]
+	if !ok {
+		return nil, nil
+	}
+
+	bid, ok := h.Bids[payload.Symbol]
+	if !ok {
+		return nil, nil
+	}
+
+	if ask.Price == 0 || bid.Price == 0 {
+		return nil, nil
+	}
+
+	return &ws.ListenChan{
+		Pair: pair,
+		OrderBook: market.OrderBook{
+			Time: time.Now().Unix(),
+			Asks: []market.OrderBookRow{ask},
+			Bids: []market.OrderBookRow{bid},
+		},
+	}, nil
+}
+
+func (h *MEXCHandler) updateOrderBook(payload MEXCOrderBookPayload) {
+
+	for _, v := range payload.Data.Asks {
+		if v[1] > 0 {
+			ask := market.OrderBookRow{
+				Price:       v[0],
+				Volume:      v[1],
+				AccumVolume: v[1],
+			}
+			h.Asks[payload.Symbol] = ask
+		}
+	}
+
+	for _, v := range payload.Data.Bids {
+		if v[1] > 0 {
+			bid := market.OrderBookRow{
+				Price:       v[0],
+				Volume:      v[1],
+				AccumVolume: v[1],
+			}
+			h.Bids[payload.Symbol] = bid
+		}
+	}
+}
+
+func mexcPairToMarketPair(in string) (market.Pair, error) {
+	const op = "mexcPairToMarketPair"
+	pairArray := strings.Split(in, "_")
+	if len(pairArray) != 2 {
+		return market.Pair{}, ez.New(op, ez.EINVALID, "invalid pair", nil)
+	}
+	return market.Pair{
+		Base:  market.Asset{Symbol: pairArray[0]},
+		Quote: market.Asset{Symbol: pairArray[1]},
+	}, nil
 }
 
 func getRequest(pair market.Pair, channel string) ([]byte, error) {
